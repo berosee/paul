a8c983cf9af021e0241a6c3ee903f42c83114a35
cha <rose3470@gmail.com>
2026-03-09 21:41:30 +0900

    202603091

---
+++ playlist_data.js
@@ -0,0 +1,472 @@
+const playlists = {
+    "enka": [
+        {
+            "title": "01. Sonata Pathetique",
+            "url": "music/enka/01. Sonata Pathetique.flac"
+        },
+        {
+            "title": "02. Aranjuez",
+            "url": "music/enka/02. Aranjuez.flac"
+        },
+        {
+            "title": "03. Les Parapluies De Cherbourg",
+            "url": "music/enka/03. Les Parapluies De Cherbourg.flac"
+        },
+        {
+            "title": "04. Jeux Interdits",
+            "url": "music/enka/04. Jeux Interdits.flac"
+        },
+        {
+            "title": "05. La Boheme",
+            "url": "music/enka/05. La Boheme.flac"
+        },
+        {
+            "title": "06. I Like Chopin",
+            "url": "music/enka/06. I Like Chopin.flac"
+        },
+        {
+            "title": "07. 13 Jours En France",
+            "url": "music/enka/07. 13 Jours En France.flac"
+        },
+        {
+            "title": "08. Apres Toi",
+            "url": "music/enka/08. Apres Toi.flac"
+        },
+        {
+            "title": "09. Comme J'ai Toujours Envie D'Aimer",
+            "url": "music/enka/09. Comme J'ai Toujours Envie D'Aimer.flac"
+        },
+        {
+            "title": "10. Qui Saura ",
+            "url": "music/enka/10. Qui Saura .flac"
+        },
+        {
+            "title": "11. Mother Of Mine",
+            "url": "music/enka/11. Mother Of Mine.flac"
+        },
+        {
+            "title": "12. Hymne A L'Amour",
+            "url": "music/enka/12. Hymne A L'Amour.flac"
+        },
+        {
+            "title": "13. Rain And Tears",
+            "url": "music/enka/13. Rain And Tears.flac"
+        },
+        {
+            "title": "14. Bilitis",
+            "url": "music/enka/14. Bilitis.flac"
+        },
+        {
+            "title": "15. Papillon",
+            "url": "music/enka/15. Papillon.flac"
+        },
+        {
+            "title": "16. Pearl Fisher",
+            "url": "music/enka/16. Pearl Fisher.flac"
+        },
+        {
+            "title": "17. Song For Anna",
+            "url": "music/enka/17. Song For Anna.flac"
+        },
+        {
+            "title": "18. Piano Ballade",
+            "url": "music/enka/18. Piano Ballade.flac"
+        },
+        {
+            "title": "19. Je Pars",
+            "url": "music/enka/19. Je Pars.flac"
+        },
+        {
+            "title": "20. Un Banc, Un Arbre, Une Rue",
+            "url": "music/enka/20. Un Banc, Un Arbre, Une Rue.flac"
+        },
+        {
+            "title": "21. Yesterday",
+            "url": "music/enka/21. Yesterday.flac"
+        },
+        {
+            "title": "22. Romeo Et Juliette",
+            "url": "music/enka/22. Romeo Et Juliette.flac"
+        },
+        {
+            "title": "23. Le Piano Sur La Vague",
+            "url": "music/enka/23. Le Piano Sur La Vague.flac"
+        },
+        {
+            "title": "24. Vivre Pour Vivre",
+            "url": "music/enka/24. Vivre Pour Vivre.flac"
+        },
+        {
+            "title": "25. La Reine De Saba",
+            "url": "music/enka/25. La Reine De Saba.flac"
+        },
+        {
+            "title": "26. The Windmills of Your Mind",
+            "url": "music/enka/26. The Windmills of Your Mind.flac"
+        },
+        {
+            "title": "27. Soleado",
+            "url": "music/enka/27. Soleado.flac"
+        },
+        {
+            "title": "28. Une Petite Cantate",
+            "url": "music/enka/28. Une Petite Cantate.flac"
+        },
+        {
+            "title": "29. Le Piano De L'automne",
+            "url": "music/enka/29. Le Piano De L'automne.flac"
+        },
+        {
+            "title": "30. Le Temps Des Fleurs",
+            "url": "music/enka/30. Le Temps Des Fleurs.flac"
+        },
+        {
+            "title": "31. Liszt - Liebestraum No. 3 in A flat",
+            "url": "music/enka/31. Liszt - Liebestraum No. 3 in A flat.flac"
+        },
+        {
+            "title": "32. Mama (Bono; Monty)",
+            "url": "music/enka/32. Mama (Bono; Monty).flac"
+        },
+        {
+            "title": "33. Comme Toujours",
+            "url": "music/enka/33. Comme Toujours.flac"
+        },
+        {
+            "title": "34. Brasilia Carnaval",
+            "url": "music/enka/34. Brasilia Carnaval.flac"
+        },
+        {
+            "title": "35. My House And The River",
+            "url": "music/enka/35. My House And The River.flac"
+        },
+        {
+            "title": "36. Sonia",
+            "url": "music/enka/36. Sonia.flac"
+        },
+        {
+            "title": "37. San Francisco",
+            "url": "music/enka/37. San Francisco.flac"
+        },
+        {
+            "title": "38. Chanson D'amour",
+            "url": "music/enka/38. Chanson D'amour.flac"
+        },
+        {
+            "title": "39. Zaide",
+            "url": "music/enka/39. Zaide.flac"
+        },
+        {
+            "title": "40. Chirpy Chirpy (Cheep Cheep)",
+            "url": "music/enka/40. Chirpy Chirpy (Cheep Cheep).flac"
+        },
+        {
+            "title": "41. Eleanor Rigby",
+            "url": "music/enka/41. Eleanor Rigby.flac"
+        },
+        {
+            "title": "42. Contigo Mi Vida",
+            "url": "music/enka/42. Contigo Mi Vida.flac"
+        },
+        {
+            "title": "43. Czardas",
+            "url": "music/enka/43. Czardas.flac"
+        },
+        {
+            "title": "44. Iberia Siempre Iberia",
+            "url": "music/enka/44. Iberia Siempre Iberia.flac"
+        },
+        {
+            "title": "45. Memory",
+            "url": "music/enka/45. Memory.flac"
+        },
+        {
+            "title": "46. Goodbye My Love Goodbye",
+            "url": "music/enka/46. Goodbye My Love Goodbye.flac"
+        },
+        {
+            "title": "47. My Reason",
+            "url": "music/enka/47. My Reason.flac"
+        },
+        {
+            "title": "48. Pegase",
+            "url": "music/enka/48. Pegase.flac"
+        },
+        {
+            "title": "49. Last Tango In Paris",
+            "url": "music/enka/49. Last Tango In Paris.flac"
+        },
+        {
+            "title": "50. This is My Song",
+            "url": "music/enka/50. This is My Song.flac"
+        },
+        {
+            "title": "51. Katioucha",
+            "url": "music/enka/51. Katioucha.flac"
+        },
+        {
+            "title": "52. La Bikina",
+            "url": "music/enka/52. La Bikina.flac"
+        },
+        {
+            "title": "53. Melancolie (Melody Lady)",
+            "url": "music/enka/53. Melancolie (Melody Lady).flac"
+        },
+        {
+            "title": "54. Inch Allah (Adamo)",
+            "url": "music/enka/54. Inch Allah (Adamo).flac"
+        },
+        {
+            "title": "55. Io Che Non Vivo",
+            "url": "music/enka/55. Io Che Non Vivo.flac"
+        },
+        {
+            "title": "56. Last Summer Day",
+            "url": "music/enka/56. Last Summer Day.flac"
+        },
+        {
+            "title": "57. Paris Ballade",
+            "url": "music/enka/57. Paris Ballade.flac"
+        },
+        {
+            "title": "58. The Bird Of Wounds (Nagekidori)",
+            "url": "music/enka/58. The Bird Of Wounds (Nagekidori).flac"
+        },
+        {
+            "title": "59. Meme Si Tu Revenais Love in Every Room",
+            "url": "music/enka/59. Meme Si Tu Revenais Love in Every Room.flac"
+        },
+        {
+            "title": "60. Une Belle Histoire",
+            "url": "music/enka/60. Une Belle Histoire.flac"
+        },
+        {
+            "title": "61. On Ne Vit Pas Sans Se Dire Adieu",
+            "url": "music/enka/61. On Ne Vit Pas Sans Se Dire Adieu.flac"
+        }
+    ],
+    "jazz": [
+        {
+            "title": "금잔디 - 고장난벽시계",
+            "url": "music/jazz/금잔디 - 고장난벽시계.mp3"
+        },
+        {
+            "title": "금잔디 - 남자라는이유로",
+            "url": "music/jazz/금잔디 - 남자라는이유로.mp3"
+        },
+        {
+            "title": "금잔디 - 네박자",
+            "url": "music/jazz/금잔디 - 네박자.mp3"
+        },
+        {
+            "title": "금잔디 - 안동역에서",
+            "url": "music/jazz/금잔디 - 안동역에서.mp3"
+        },
+        {
+            "title": "금잔디 - 일편단심",
+            "url": "music/jazz/금잔디 - 일편단심.mp3"
+        }
+    ],
+    "old": [
+        {
+            "title": "01. Love Is Blue",
+            "url": "music/old/01. Love Is Blue.flac"
+        },
+        {
+            "title": "02. El Condor Pasa",
+            "url": "music/old/02. El Condor Pasa.flac"
+        },
+        {
+            "title": "03. Toccata",
+            "url": "music/old/03. Toccata.flac"
+        },
+        {
+            "title": "04. Tombe La Niege",
+            "url": "music/old/04. Tombe La Niege.flac"
+        },
+        {
+            "title": "05. Delilah",
+            "url": "music/old/05. Delilah.flac"
+        },
+        {
+            "title": "06. El Bimbo",
+            "url": "music/old/06. El Bimbo.flac"
+        },
+        {
+            "title": "07. Veins - Veins",
+            "url": "music/old/07. Veins - Veins.flac"
+        },
+        {
+            "title": "08. Mamy Blue",
+            "url": "music/old/08. Mamy Blue.flac"
+        },
+        {
+            "title": "09. Penelope",
+            "url": "music/old/09. Penelope.flac"
+        },
+        {
+            "title": "10. Amore Grande, Amore Libero",
+            "url": "music/old/10. Amore Grande, Amore Libero.flac"
+        },
+        {
+            "title": "11. Paloma Blanca",
+            "url": "music/old/11. Paloma Blanca.flac"
+        },
+        {
+            "title": "12. La Nuit",
+            "url": "music/old/12. La Nuit.flac"
+        },
+        {
+            "title": "13. Il Silenzio Bonsoir Mon Amour",
+            "url": "music/old/13. Il Silenzio Bonsoir Mon Amour.flac"
+        },
+        {
+            "title": "14. L`Enfant Au Tambour (The little Drummer Boy) ",
+            "url": "music/old/14. L`Enfant Au Tambour (The little Drummer Boy) .flac"
+        },
+        {
+            "title": "15. Minuetto",
+            "url": "music/old/15. Minuetto.flac"
+        },
+        {
+            "title": "16. Un Homme Et Une Femme",
+            "url": "music/old/16. Un Homme Et Une Femme.flac"
+        },
+        {
+            "title": "17. Love Story",
+            "url": "music/old/17. Love Story.flac"
+        },
+        {
+            "title": "18. Sympathy",
+            "url": "music/old/18. Sympathy.flac"
+        },
+        {
+            "title": "19. Petite Melodie",
+            "url": "music/old/19. Petite Melodie.flac"
+        },
+        {
+            "title": "20. La Paloma",
+            "url": "music/old/20. La Paloma.flac"
+        },
+        {
+            "title": "21. Godfather",
+            "url": "music/old/21. Godfather.flac"
+        },
+        {
+            "title": "22. Nocturnae",
+            "url": "music/old/22. Nocturnae.flac"
+        },
+        {
+            "title": "23. Butterfly",
+            "url": "music/old/23. Butterfly.flac"
+        },
+        {
+            "title": "24. Raindrops Keep Fallin' On My Head",
+            "url": "music/old/24. Raindrops Keep Fallin' On My Head.flac"
+        },
+        {
+            "title": "25. Speak Softly Love",
+            "url": "music/old/25. Speak Softly Love.flac"
+        },
+        {
+            "title": "26. Symphony No. 40 1st Movement",
+            "url": "music/old/26. Symphony No. 40 1st Movement.flac"
+        },
+        {
+            "title": "27. Isadora",
+            "url": "music/old/27. Isadora.flac"
+        },
+        {
+            "title": "28. Il Cuore e Uno Zingaro",
+            "url": "music/old/28. Il Cuore e Uno Zingaro.flac"
+        },
+        {
+            "title": "29. Parle Plus Bas",
+            "url": "music/old/29. Parle Plus Bas.flac"
+        },
+        {
+            "title": "30. Hey Jude",
+            "url": "music/old/30. Hey Jude.flac"
+        },
+        {
+            "title": "31. Phantom Of The Opera",
+            "url": "music/old/31. Phantom Of The Opera.flac"
+        },
+        {
+            "title": "32. What Is A Youth",
+            "url": "music/old/32. What Is A Youth.flac"
+        },
+        {
+            "title": "33. Feelings",
+            "url": "music/old/33. Feelings.flac"
+        },
+        {
+            "title": "34. Aloette",
+            "url": "music/old/34. Aloette.flac"
+        },
+        {
+            "title": "35. Emmanuelle",
+            "url": "music/old/35. Emmanuelle.flac"
+        },
+        {
+            "title": "36. Je T'Aime Moi Non Plus",
+            "url": "music/old/36. Je T'Aime Moi Non Plus.flac"
+        },
+        {
+            "title": "37. Adieu A La Nuit",
+            "url": "music/old/37. Adieu A La Nuit.flac"
+        },
+        {
+            "title": "38. Hungarian Dance",
+            "url": "music/old/38. Hungarian Dance.flac"
+        },
+        {
+            "title": "39. Elise",
+            "url": "music/old/39. Elise.flac"
+        },
+        {
+            "title": "40. Holydays",
+            "url": "music/old/40. Holydays.flac"
+        },
+        {
+            "title": "41. Serenade To Summertime",
+            "url": "music/old/41. Serenade To Summertime.flac"
+        },
+        {
+            "title": "42. Penelope",
+            "url": "music/old/42. Penelope.flac"
+        },
+        {
+            "title": "43. Merci Cherie (Jurgens; Horbiger; Carrere; Salvet)flac",
+            "url": "music/old/43. Merci Cherie (Jurgens; Horbiger; Carrere; Salvet)flac.flac"
+        },
+        {
+            "title": "44. La Pioggia",
+            "url": "music/old/44. La Pioggia.flac"
+        },
+        {
+            "title": "45. Minuetto",
+            "url": "music/old/45. Minuetto.flac"
+        },
+        {
+            "title": "46. My Heart Will Gone On",
+            "url": "music/old/46. My Heart Will Gone On.flac"
+        },
+        {
+            "title": "47. Yesterday Once More",
+            "url": "music/old/47. Yesterday Once More.flac"
+        },
+        {
+            "title": "48. Paul Mauriat - 01 Amore Grande Amore Libero",
+            "url": "music/old/48. Paul Mauriat - 01 Amore Grande Amore Libero.flac"
+        },
+        {
+            "title": "49. La Nuit",
+            "url": "music/old/49. La Nuit.flac"
+        },
+        {
+            "title": "50. 돌아와요 부산항에",
+            "url": "music/old/50. 돌아와요 부산항에.flac"
+        }
+    ]
+};
